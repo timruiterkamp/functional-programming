02e1283f88ed1f5b68f41b10dd575a8214451af7
timruiterkamp <timruiterkamp@gmail.com>
2018-11-04 14:15:55 +0100

    fixed json generator returning array with arrays instead of objects

--- helpers/filterHelper.js
+++ helpers/filterHelper.js
@@ -4,16 +4,11 @@ findObject = (res, name) => jp.query(res, `$..${name}`)
 findValue = item => (item[0] && item[0][0]._ ? item[0][0]._ : item)
 filterStringOnSpecChars = item => item.split(':')[0].split('/')[0]
 filterValuesInObject = res => {
-	return Object.values(res).map(x => {
-		const values = []
-		const { author, title, language } = x
-		values.push({
-			author: author && findValue(author) ? findValue(author) : '',
-			title: title && findValue(title) ? findValue(title) : '',
-			language: language && findValue(language) ? findValue(language) : ''
-		})
-		return values
-	})
+	return Object.values(res).map(({ author, title, language }) => ({
+		author: author && findValue(author) ? findValue(author) : '',
+		title: title && findValue(title) ? findValue(title) : '',
+		language: language && findValue(language) ? findValue(language) : ''
+	}))
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const obaApi = require('./api/obaApi')
-const filterValuesInObject = require('./helpers/filterHelper')
-	.filterValuesInObject
+const filter = require('./helpers/filterHelper')
 const scrape = require('./scraper/scraper').findPriceByItem
 const newObj = require('./helpers/objectHelper').createNewObject
 var fs = require('fs')
@@ -23,24 +22,35 @@ const filterQuery = {
 	librarian: true,
 	facet: 'genre(avonturenroman)'
 }
-const filterKey = ''
 
-api.getAll('search', filterQuery, filterKey)
+api.getAll('search', filterQuery)
 	.then(response => (api.response = response.data))
 	.then(apiData =>
 		apiData.map(items => newObj(items, ['author', 'title', 'language']))
 	)
-	.then(res => (filteredValues = filterValuesInObject(res)))
+	.then(res => (filteredValues = filter.filterValuesInObject(res)))
+	// .then(res => {
+	// 	const filtereuh = res.map(item => {
+	// 		for (const val of Object.values(item)) {
+	// 			console.log(val)
+	// 		}
+	// 	})
+	// 	console.log(filtereuh)
+	// 	return res
+	// })
 	.then(res => scrape('Het geheim van pater Brugman'))
+	.then(res =>
+		app.get('/', (req, res) => {
+			fs.writeFile(
+				'./api/cleanBookData.json',
+				JSON.stringify(filteredValues),
+				'utf8',
+				err => console.error('write file kan niet', err)
+			)
+
+			res.json(filteredValues)
+		})
+	)
 	.catch(err => console.error('doet t niet'))
 
-app.get('/', (req, res) => {
-	fs.writeFile(
-		'./api/cleanBookData.json',
-		JSON.stringify(filteredValues),
-		'utf8',
-		err => console.error('write file kan niet', err)
-	)
-
-	res.json(filteredValues)
-}).listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`))
