a205b7a71c5a5dfe9479483245b6efd3a67a9e44
timruiterkamp <timruiterkamp@gmail.com>
2018-11-05 15:34:27 +0100

    updated index to not instantly firing the scraper

--- index.js
+++ index.js
@@ -9,50 +9,69 @@ const {
 } = require('./helpers/filterHelper')
 const { createNewObject: newObj } = require('./helpers/objectHelper')
 
-initScraper().then(scraper => {
-	const app = express()
-	const port = 1337
-	const api = new obaApi({
-		publicKey: process.env.PUBLIC_KEY,
-		secret: process.env.SECRET,
-		url: 'https://zoeken.oba.nl/api/v1/'
+const app = express()
+const port = 1337
+
+app.get('/', (req, res) => {
+	readBookJson('./api/cleanBookData.json', data => {
+		if (data) {
+			res.json(JSON.parse(data))
+		} else {
+			res.write('<h2>Loading</h2>')
+		}
 	})
-	const filterQuery = {
-		q: 'roman',
-		refine: true,
-		librarian: true,
-		lang: 'nl',
-		facet: ['genre(avonturenroman)', 'language(dut)']
-	}
+}).listen(port, () => console.log(`Listening on port ${port}`))
 
-	api.getAll('search', filterQuery)
-		.then(response => (api.response = response.data))
-		.then(apiData =>
-			apiData.map(items => newObj(items, ['author', 'title', 'language']))
-		)
-		.then(dirtyBooks => filterValuesInObject(dirtyBooks))
-		.then(
-			cleanBooksWithDirtyTitles =>
-				(filteredData = cleanBooksWithDirtyTitles.map(book => ({
-					...book,
-					title: filterStringOnSpecChars(book.title)
-				})))
-		)
-		.then(cleanBooks => scraper.findPricesByItems(cleanBooks))
-		.then(booksWithPrices => {
-			console.log(booksWithPrices)
-			return (finalizedBooksDataset = booksWithPrices)
+function readBookJson(filePath, cb) {
+	fs.readFile(filePath, 'utf8', function(err, data) {
+		if (err) {
+			startScraping()
+			console.log('geen boeken data beschikbaar')
+		}
+		cb(data)
+	})
+}
+
+function startScraping() {
+	initScraper().then(scraper => {
+		const api = new obaApi({
+			publicKey: process.env.PUBLIC_KEY,
+			secret: process.env.SECRET,
+			url: 'https://zoeken.oba.nl/api/v1/'
 		})
-		.catch(err => console.error('doet t niet'))
+		const filterQuery = {
+			q: 'roman',
+			refine: true,
+			librarian: true,
+			lang: 'nl',
+			facet: ['genre(avonturenroman)', 'language(dut)']
+		}
 
-	app.get('/', (req, res) => {
-		fs.writeFile(
-			'./api/cleanBookData.json',
-			JSON.stringify(finalizedBooksDataset),
-			'utf8',
-			err => console.error('write file kan niet', err)
-		)
-
-		res.json(finalizedBooksDataset)
-	}).listen(port, () => console.log(`Listening on port ${port}`))
-})
+		api.getAll('search', filterQuery)
+			.then(response => (api.response = response.data))
+			.then(apiData =>
+				apiData.map(items =>
+					newObj(items, ['author', 'title', 'language'])
+				)
+			)
+			.then(dirtyBooks => filterValuesInObject(dirtyBooks))
+			.then(
+				cleanBooksWithDirtyTitles =>
+					(filteredData = cleanBooksWithDirtyTitles.map(book => ({
+						...book,
+						title: filterStringOnSpecChars(book.title)
+					})))
+			)
+			.then(cleanBooks => scraper.findPricesByItems(cleanBooks))
+			.then(booksWithPrices => {
+				console.log(booksWithPrices)
+				fs.writeFile(
+					'./api/cleanBookData.json',
+					JSON.stringify(booksWithPrices),
+					'utf8',
+					err => console.error('write file kan niet', err)
+				)
+			})
+			.catch(err => console.error('doet t niet'))
+	})
+}
